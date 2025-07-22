1b83639e37892362e5ae9a03933eda55ab12e43e
shivakumarchirra <chirrashivakumar5@gmail.com>
2025-07-22 21:30:11 +0530

    Initial Commit

---
+++ To Do List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To Do List
+//
+//  Created by shivakumar chirra on 22/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by shivakumar chirra on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
